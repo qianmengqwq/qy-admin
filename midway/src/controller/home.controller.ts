a7a92534be949c5c24cab6d221dcbf4518523248
sayoriqwq <2531600563@qq.com>
2024-05-06 23:54:04 +0800

    feat: 公共错误

--- midway/src/controller/home.controller.ts
+++ midway/src/controller/home.controller.ts
@@ -1,62 +1,6 @@
-// // ./src/controller/home.controller.ts
-// import { Controller, Get, Inject } from '@midwayjs/core';
-// import { InjectEntityModel } from '@midwayjs/typeorm';
-// import { User } from '../entity/user.entity';
-// import { Repository } from 'typeorm';
-// import { RedisService } from '@midwayjs/redis';
-// import { MidwayI18nService } from '@midwayjs/i18n';
-
-// @Controller('/')
-// export class HomeController {
-//   // 自动注入模型
-//   @InjectEntityModel(User)
-//   userModel: Repository<User>;
-
-//   @Inject()
-//   redisService: RedisService;
-//   @Inject()
-//   i18nService: MidwayI18nService;
-//   @Get('/')
-//   async home(): Promise<string> {
-//     // 查询user表数据
-//     // return await this.userModel.find();
-//     // await this.redisService.set('foo', 'bar');
-//     // return await this.redisService.get('foo');
-//     return this.i18nService.translate('hello', {
-//       locale: 'zh_CN',
-//     });
-//   }
-// }
-// src/controller/home.controller.ts
-// import { Body, Controller, Post } from '@midwayjs/core';
-// import { UserDTO } from '../dto/user';
-
-// @Controller('/')
-// export class HomeController {
-//   @Post('/')
-//   async home(@Body() user: UserDTO): Promise<void> {
-//     console.log(user);
-//   }
-// }
-
-// src/controller/home.controller.ts
-// import { Controller, Inject, Post } from '@midwayjs/core';
-// import { ILogger } from '@midwayjs/logger';
-// import { CommonError } from '../common/common.error';
-
-// @Controller('/')
-// export class HomeController {
-//   @Inject()
-//   logger: ILogger;
-
-//   @Post('/')
-//   async home(): Promise<void> {
-//     throw new CommonError('error');
-//   }
-// }
-import { Body, Controller, Inject, Post } from '@midwayjs/core';
-import { UserDTO } from '../dto/user';
+import { Controller, Inject, Post } from '@midwayjs/core';
 import { ILogger } from '@midwayjs/logger';
+import { R } from '../common/base.error.util';
 
 @Controller('/')
 export class HomeController {
@@ -64,8 +8,7 @@ export class HomeController {
   logger: ILogger;
 
   @Post('/')
-  async home(@Body() user: UserDTO): Promise<void> {
-    this.logger.info('hello');
-    console.log(user);
+  async home(): Promise<void> {
+    throw R.error('error');
   }
 }
--- midway/src/dto/user.ts
+++ midway/src/dto/user.ts
@@ -1,15 +1,16 @@
 // src/dto/user.ts
 import { ApiProperty } from '@midwayjs/swagger';
 import { Rule, RuleType } from '@midwayjs/validate';
+import { R } from '../common/base.error.util';
 
 export class UserDTO {
   @ApiProperty({ description: 'id' })
   @Rule(RuleType.allow(null))
   id?: string;
   @ApiProperty({ description: '姓名' })
-  @Rule(RuleType.string().required().error(new Error('姓名不能为空')))
+  @Rule(RuleType.string().required().error(R.validateError('姓名不能为空')))
   name: string;
   @ApiProperty({ description: '年龄' })
-  @Rule(RuleType.number().required().error(new Error('年龄不能为空')))
+  @Rule(RuleType.number().required().error(R.validateError('年龄不能为空')))
   age: number;
 }
